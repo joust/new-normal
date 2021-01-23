4a5374bac274d748de4d3ae0070635e8183e4035
joust <github@joust.de>
2021-01-23 22:51:56 +0100

    exclusions

--- sw.js
+++ sw.js
@@ -3,21 +3,13 @@ console.log('WORKER: executing.')
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var version = 'v1::'
+var version = 'v3::'
 
 /* These resources will be downloaded and cached by the service worker
    during the installation process. If any resource fails to be downloaded,
    then the service worker won't be installed either.
 */
 var offlineFundamentals = [
-  '',
-  'style.css',
-  'crocodile.css',
-  'opensans.css',
-  'pyro.css',
-  'bingo.js',
-  'flipside.js',
-  'fastclick.js'
 ]
 
 /* The install event fires when the service worker is first installed.

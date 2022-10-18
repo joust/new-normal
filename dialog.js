fd6b148b7853ea6f8edbf270616105ce86090074
joust <github@joust.de>
2022-10-18 13:24:51 +0200

    working dialog

--- dialog.js
+++ dialog.js
@@ -1,168 +1,354 @@
-document.head.insertAdjacentHTML(
-  'afterBegin',
-  `<style>
-    .u1x-modal .-buttons {
+/**
+ * Message Box web component (based on a pen by https://codepen.io/takaneichinose)
+ */
+class MessageBoxElement extends HTMLElement {
+  constructor() {
+    super()
+    this.build()
+  }
+
+  build() {
+      this.attachShadow({
+          mode: 'open'
+        })
+        //---------- Build the modal window
+      const modalWindow = document.createElement('div')
+      modalWindow.classList.add('msg-box-modal')
+        //---------- Build the dialog window
+      const dialogElm = document.createElement('div')
+      const dialogHeaderElm = document.createElement('div')
+      const dialogBodyElm = document.createElement('div')
+      const dialogFooterElm = document.createElement('div')
+      dialogElm.classList.add('msg-box-dialog')
+      dialogHeaderElm.classList.add('msg-box-dialog-header')
+      dialogBodyElm.classList.add('msg-box-dialog-body')
+      dialogFooterElm.classList.add('msg-box-dialog-footer')
+      dialogBodyElm.append(document.createElement('p'))
+      dialogElm.append(dialogHeaderElm, dialogBodyElm, dialogFooterElm)
+      modalWindow.append(dialogElm)
+        //---------- Styling
+      const style = document.createElement('style')
+      style.textContent = this.setStyle()
+      this.shadowRoot.append(style, modalWindow)
+        //---------- Set default
+      this.setDefault()
+    }
+
+  setStyle() {
+      const padding = '1em'
+      return `
+      .msg-box-modal {
+        font-family: inherit;
+        font-size: inherit;
+        width: 100%;
+        height: 100%;
         display: flex;
-        flex-wrap: wrap;
-        justify-content: flex-end;
-        gap: .5rem;
-        margin-top: 1rem;
-    }
-    </style>`
-)
-
-class Dialog {
-  constructor(options) {
-    const template = document.createElement('template')
-    template.innerHTML =
-      `<dialog class="u1x-modal">
-            <form method=dialog>
-                ${options.body}
-                ${options.buttons ? '<div class=-buttons focusgroup></div> ' : ''}
-            </form>
-        </dialog>`
-    const element = this.element = template.content.firstChild
-    const btnCont = element.querySelector('.-buttons')
-    options.buttons && options.buttons.forEach((btn, i) => {
-      const el = document.createElement('button')
-      el.innerHTML = btn.title
-      el.value = btn.value
-      el.addEventListener('click', e => {
-        btn.then && btn.then.call(this, e)
-      })
-      btnCont.appendChild(el)
-      if (i === 0) setTimeout(() => el.focus())
-    })
-    options.init && options.init(this.element)
-  }
-  show() {
-    const element = this.element
-    console.log(element)
-    document.body.appendChild(element)
-    element.showModal()
-    return new Promise((resolve, reject) => {
-      element.addEventListener('close', () => {
-        resolve(this.value)
-        element.remove()
-      })
-    })
-  }
-}
-
-function toOptions(text) {
-  if (typeof text === 'string') {
-    return {
-      body: htmlEntities(text)
-    }
-  }
-  return text
-}
-
-
-function alert(text) {
-  const options = toOptions(text)
-  options.buttons = [{
-    title: 'OK'
-  }]
-  return new Dialog(options).show()
-}
-
-function confirm(text) {
-  const options = toOptions(text)
-  options.buttons = [
-    {
-      title: 'OK',
-      then() {
-        dialog.value = true
+        justify-content: center;
+        align-items: center;
+        overflow: auto;
+        position: fixed;
+        top: 0;
+        left: 0;
       }
-    },
-    {
-      title: t('Cancel')
-    }
-    ]
-  const dialog = new Dialog(options)
-  dialog.value = false
-  return dialog.show()
-}
-
-function prompt(text, initial) {
-  const options = toOptions(text)
-  options.body = `<label>${options.body}<input style="width:100%; display:block; margin-top:.5rem"></label>`
-  options.buttons = [
-    {
-      title: 'OK',
-      then() {
-        dialog.value = input.value
+      
+      .msg-box-dialog {
+        width: calc(100% - 2em);
+        max-width: 40vw;
+        overflow: hidden;
+        box-sizing: border-box;
+        box-shadow: 0 0.5em 1em rgba(0, 0, 0, 0.5);
+        border-radius: 0.3em;
+        animation: msg-box-dialog-show 265ms cubic-bezier(0.18, 0.89, 0.32, 1.28)
       }
-    },
-    {
-      title: t('Cancel')
+      
+      .msg-box-dialog.msg-box-dialog-hide {
+        opacity: 0;
+        animation: msg-box-dialog-hide 265ms ease-in;
+      }
+      
+      @keyframes msg-box-dialog-show {
+        0% {
+          opacity: 0;
+          transform: translateY(-100%);
+        }
+        100% {
+          opacity: 1;
+          transform: translateX(0);
+        }
+      }
+      
+      @keyframes msg-box-dialog-hide {
+        0% {
+          opacity: 1;
+          transform: translateX(0);
+        }
+        100% {
+          opacity: 0;
+          transform: translateY(-50%);
+        }
+      }
+      
+      .msg-box-dialog-header {
+        color: inherit;
+        background-color: rgba(0, 0, 0, 0.05);
+        padding: ${padding};
+        border-bottom: solid 1px rgba(0, 0, 0, 0.15);
+      }
+      
+      .msg-box-dialog-body {
+        color: inherit;
+        padding: ${padding};
+      }
+      
+      .msg-box-dialog-body > p {
+        color: inherit;
+        padding: 0;
+        margin: 0;
+      }
+      
+      .msg-box-dialog-footer {
+        color: inherit;
+        display: flex;
+        justify-content: stretch;
+      }
+      
+      .msg-box-dialog-button {
+        color: inherit;
+        font-family: inherit;
+        font-size: inherit;
+        background-color: rgba(0, 0, 0, 0);
+        width: 100%;
+        padding: 1em;
+        border: none;
+        border-top: solid 1px rgba(0, 0, 0, 0.15);
+        outline: 0;
+        border-radius: 0px;
+        transition: background-color 225ms ease-out;
+      }
+      
+      .msg-box-dialog-button:focus {
+        background-color: rgba(0, 0, 0, 0.05);
+      }
+      
+      .msg-box-dialog-button:active {
+        background-color: rgba(0, 0, 0, 0.15);
+      }
+      
+      .msg-box-dialog-textbox {
+        color: inherit;
+        font-family: inherit;
+        font-size: inherit;
+        width: 100%;
+        padding: 0.5em;
+        border: solid 1px rgba(0, 0, 0, 0.15);
+        margin-top: ${padding};
+        outline: 0;
+        box-sizing: border-box;
+        border-radius: 0;
+        box-shadow: 0 0 0 0 rgba(13, 134, 255, 0.5);
+        transition: box-shadow 125ms ease-out, border 125ms ease-out;
+      }
+      
+      .msg-box-dialog-textbox:focus {
+        border: solid 1px rgba(13, 134, 255, 0.8);
+        box-shadow: 0 0 0.1em 0.2em rgba(13, 134, 255, 0.5);
+      }
+      
+      @media (prefers-color-scheme: dark) {
+        .msg-box-modal {
+          background-color: rgba(31, 31, 31, 0.5);
+        }
+        
+        .msg-box-dialog {
+          color: #f2f2f2;
+          background-color: #464646;
+        }
+        
+        .msg-box-dialog-textbox {
+          background-color: #2f2f2f;
+        }
+      }
+      
+      @media (prefers-color-scheme: light) {
+        .msg-box-modal {
+          background-color: rgba(221, 221, 221, 0.5);
+        }
+        
+        .msg-box-dialog {
+          color: #101010;
+          background-color: #ffffff;
+        }
+      }
+    `
     }
-    ]
-  const dialog = new Dialog(options)
-  const input = dialog.element.querySelector('input')
-  input.value = initial
-  setTimeout(() => input.focus())
-  dialog.value = null
-  return dialog.show()
-}
-
-/*
-export function form(html){
-    const dialog = new Dialog({
-        body:html,
-        buttons:[{title:'OK',then(){
-            const form = dialog.element.querySelector('form')
-            const data = {}
-            form.querySelectorAll('input,textarea,select').forEach(el=>{
-                data[el.name] = el.value
-                if (el.type === 'checkbox') data[el.name] = el.checked ? el.value : null
-            })
-            dialog.value = data
-        }}]
+    /**
+     * Set the default value of the dialog box
+     */
+  setDefault() {
+      let content = this.dataset.content
+      let title = this.dataset.title
+      let type = this.dataset.type
+      if (typeof content === 'undefined' || typeof type === 'undefined') {
+        // Dialog will be built if these two datatypes are existing
+        // as HTML attributes. This is to prevent calling the dialog
+        // builders twice when the dialog was call from javascript.
+        return
+      }
+      if (typeof title === 'undefined') {
+        title = null
+      }
+      switch (type) {
+      case 'alert':
+        this.setAlert(content, title)
+        break
+      case 'confirm':
+        this.setConfirm(content, title)
+        break
+      case 'prompt':
+        this.setPrompt(content, title)
+        break
+      }
+    }
+    /**
+     * Put the title and the content of the dialog box.
+     */
+  createDialog(content, title) {
+      const dialogHeaderElm = this.shadowRoot.querySelector('.msg-box-dialog-header')
+      const dialogBodyElm = this.shadowRoot.querySelector('.msg-box-dialog-body > p')
+      dialogBodyElm.innerHTML = content
+      if (title === null) {
+        dialogHeaderElm.remove()
+      } else {
+        dialogHeaderElm.innerHTML = title
+      }
+    }
+    /**
+     * Execute 'animationend' event of the dialog, then dispose
+     */
+  disposeDialog() {
+      const self = this
+      const dialogElm = self.shadowRoot.querySelector('.msg-box-dialog')
+      dialogElm.classList.add('msg-box-dialog-hide')
+      dialogElm.addEventListener('animationend', function dialogElmAnimationEnd(evt) {
+        if (evt.animationName === 'msg-box-dialog-hide') {
+          dialogElm.removeEventListener('animationend', dialogElmAnimationEnd)
+          self.remove()
+        }
+      })
+    }
+    /**
+     * Creates the alert dialog element
+     */
+  setAlert(content, title) {
+      const self = this
+      self.createDialog(content, title)
+      const dialogFooterElm = self.shadowRoot.querySelector('.msg-box-dialog-footer')
+      const dialogConfirmBtn = document.createElement('button')
+      dialogConfirmBtn.classList.add('msg-box-dialog-button')
+      dialogConfirmBtn.innerText = 'OK'
+      dialogFooterElm.append(dialogConfirmBtn)
+      dialogConfirmBtn.focus()
+      return new Promise(function (resolve) {
+        dialogConfirmBtn.addEventListener('click', function dialogConfirmBtnClick() {
+          dialogConfirmBtn.removeEventListener('click', dialogConfirmBtnClick)
+          self.disposeDialog()
+          resolve(true)
+        })
+      })
+    }
+    /**
+     * Creates the confirm dialog element
+     */
+  setConfirm(content, title) {
+      const self = this
+      self.createDialog(content, title)
+      const dialogFooterElm = self.shadowRoot.querySelector('.msg-box-dialog-footer')
+      const dialogCancelBtn = document.createElement('button')
+      const dialogConfirmBtn = document.createElement('button')
+      dialogCancelBtn.classList.add('msg-box-dialog-button')
+      dialogCancelBtn.innerText = 'Cancel'
+      dialogConfirmBtn.classList.add('msg-box-dialog-button')
+      dialogConfirmBtn.innerText = 'OK'
+      dialogFooterElm.append(dialogCancelBtn, dialogConfirmBtn)
+      dialogCancelBtn.focus()
+      return new Promise(function (resolve) {
+        dialogCancelBtn.addEventListener('click', function dialogCancelBtnClick() {
+          this.removeEventListener('click', dialogCancelBtnClick)
+          self.disposeDialog()
+          resolve(false)
+        })
+        dialogConfirmBtn.addEventListener('click', function dialogCancelBtnClick() {
+          this.removeEventListener('click', dialogCancelBtnClick)
+          self.disposeDialog()
+          resolve(true)
+        })
+      })
+    }
+    /**
+     * Creates the prompt dialog element
+     */
+  setPrompt(content, title) {
+    const self = this
+    self.createDialog(content, title)
+      // Create Textbox and put into the dialog body.
+    const dialogBodyElm = self.shadowRoot.querySelector('.msg-box-dialog-body')
+    const dialogMessageTextBoxContainer = document.createElement('p')
+    const dialogMessageTextBox = document.createElement('input')
+    dialogMessageTextBox.classList.add('msg-box-dialog-textbox')
+    dialogMessageTextBox.type = 'text'
+    dialogMessageTextBoxContainer.append(dialogMessageTextBox)
+    dialogBodyElm.append(dialogMessageTextBoxContainer)
+      // Create buttons, and put into the dialog footer.
+    const dialogFooterElm = self.shadowRoot.querySelector('.msg-box-dialog-footer')
+    const dialogCancelBtn = document.createElement('button')
+    const dialogConfirmBtn = document.createElement('button')
+    dialogCancelBtn.classList.add('msg-box-dialog-button')
+    dialogCancelBtn.innerText = 'Cancel'
+    dialogConfirmBtn.classList.add('msg-box-dialog-button')
+    dialogConfirmBtn.innerText = 'OK'
+    dialogFooterElm.append(dialogCancelBtn, dialogConfirmBtn)
+    dialogMessageTextBox.focus()
+      // Prompt message textbox KeyPress event
+    function dialogMessageTextBoxKeyPress(evt) {
+      if (evt.key === 'Enter') {
+        // If Enter key has been pressed
+        dialogConfirmBtn.click()
+      }
+    }
+    dialogMessageTextBox.addEventListener('keypress', dialogMessageTextBoxKeyPress)
+    return new Promise(function (resolve) {
+      dialogCancelBtn.addEventListener('click', function dialogCancelBtnClick() {
+        this.removeEventListener('click', dialogCancelBtnClick)
+        dialogMessageTextBox.removeEventListener('keypress', dialogMessageTextBoxKeyPress)
+        self.disposeDialog()
+        resolve(null)
+      })
+      dialogConfirmBtn.addEventListener('click', function dialogCancelBtnClick() {
+        this.removeEventListener('click', dialogCancelBtnClick)
+        dialogMessageTextBox.removeEventListener('keypress', dialogMessageTextBoxKeyPress)
+        self.disposeDialog()
+        resolve(dialogMessageTextBox.value)
+      })
     })
-    return dialog.show()
-}
-*/
-
-
-// helper functions
-function htmlEntities(str) {
-  return String(str).replace(/&/g, '&amp').replace(/</g, '&lt').replace(/>/g, '&gt').replace(/"/g, '&quot')
-}
-
-function t(v) {
-  function getlang() {
-    return navigator.language.substring(0, 2)
   }
-  const text = {
-    'Cancel': {
-      'da': 'Annuller',
-      'de': 'Abbrechen',
-      'fr': 'Annuler',
-      'es': 'Cancelar',
-      'it': 'Annulla',
-      'pt': 'Cancelar',
-      'pt-br': 'Cancelar',
-      'pl': 'Anuluj',
-      'ru': 'Отмена',
-      'ja': 'キャンセル',
-      'ko': '취소',
-      'zh': '取消',
-      'nl': 'Annuleren',
-      'tr': 'İptal'
-    }
-  }
-  return text[v][getlang()] || v
+}
+// Define the custom element as a web component
+customElements.define('msg-box', MessageBoxElement)
+
+function alert(content, title = null) {
+  const dialogBox = document.createElement('msg-box')
+  document.body.appendChild(dialogBox)
+  return dialogBox.setAlert(content, title)
 }
 
-// close dialog on backdrop-click if it has the backdropClose-class
-addEventListener('click', e => {
-  const el = e.target
-  if (el.tagName !== 'DIALOG') return
-  if (!el.classList.contains('backdropClose')) return
-  const rect = el.getBoundingClientRect()
-  let isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height && rect.left <= event.clientX && event.clientX <= rect.left + rect.width)
-  if (isInDialog) return
-  el.close()
-})
+function confirm(content, title = null) {
+  const dialogBox = document.createElement('msg-box')
+  document.body.appendChild(dialogBox)
+  return dialogBox.setConfirm(content, title)
+}
+
+function prompt(content, title = null) {
+  const dialogBox = document.createElement('msg-box')
+  document.body.appendChild(dialogBox)
+  return dialogBox.setPrompt(content, title)
+}

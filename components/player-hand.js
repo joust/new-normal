70a3b7ab5cc8932ccd6bc34ee1e6d4c3b483437b
joust <github@joust.de>
2022-11-14 13:25:02 +0100

    check

--- components/player-hand.js
+++ components/player-hand.js
@@ -175,6 +175,7 @@ class PlayerHand extends HTMLElement {
   }
 
   over(event) {
+    console.log(event.target.card || event.target.id)
     event.target && event.target.id!=='player-hand' && this.show(event.target)
   }
 
